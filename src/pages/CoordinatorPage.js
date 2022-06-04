72b6aa33c73300ae47331404d5a0167d071d2c7a
Vinutha <vinuthae24@gmail.com>
2022-06-04 16:49:23 +0530

    added indicators

--- src/pages/CoordinatorPage.js
+++ src/pages/CoordinatorPage.js
@@ -8,24 +8,37 @@ const CoordinatorPage = () => {
                 <label htmlFor="name"> Name</label>
                 <input type="text"  id="name" className="form-control" />
             </div>
+           
             <div className="mb-3">
-                <label htmlFor="Address">Address</label>
-                <textarea  id="Address" cols="30" rows="10" className="form-control"></textarea>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="contact">Mobile Number</label>
+                <label htmlFor="contact">age</label>
                 <input type="number" id="contact" className="form-control"/>
             </div>
             <div className="mb-3">
-                <label htmlFor="pname">Product Name</label>
-                <input type="text"  id="pname" className='form-control' />
+                <label htmlFor="pname">weight</label>
+                <input type="number"  id="weight" className='form-control' />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="pname">height</label>
+                <input type="number"  id="height" className='form-control' />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="pname">start_date</label>
+                <input type="date"  id="date" className='form-control' />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="pname">dosage</label>
+                <input type="number"  id="dose" className='form-control' />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="pname">amount</label>
+                <input type="number"  id="amt" className='form-control' />
             </div>
             <div className="mb-3">
                 <label htmlFor="description">product description</label>
                 <textarea  id="description" cols="30" rows="10" className="form-control"></textarea>
             </div>
             <div className="mb-3">
-                <label htmlFor="upload">upload item image</label>
+                <label htmlFor="upload">upload spreedsheet</label>
                 <input type="file" id="upload" className="form-control" />
             </div>
             <div className="mb-3">
