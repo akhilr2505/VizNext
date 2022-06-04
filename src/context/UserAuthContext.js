aae61f5bfdcba1d88a300d36e145eb8a13ae7050
Vasu Mandhanya <55338336+Vasu-Mandhanya@users.noreply.github.com>
2022-06-05 05:13:03 +0530

    FIx Navbar Issue.

--- src/context/UserAuthContext.js
+++ src/context/UserAuthContext.js
@@ -1,53 +1,64 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
+import
+  {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+    GoogleAuthProvider,
+    signInWithPopup,
+  } from "firebase/auth";
 import { auth } from "../firebase";
 
 const userAuthContext = createContext();
 
-export function UserAuthContextProvider({ children }) {
-  const [user, setUser] = useState({});
+export function UserAuthContextProvider ( { children } )
+{
+  const [ user, setUser ] = useState( {} );
 
-  function logIn(email, password) {
-    return signInWithEmailAndPassword(auth, email, password);
+  function logIn ( email, password )
+  {
+    return signInWithEmailAndPassword( auth, email, password );
   }
-  function signUp(email, password) {
-    return createUserWithEmailAndPassword(auth, email, password);
+  function signUp ( email, password )
+  {
+    return createUserWithEmailAndPassword( auth, email, password );
   }
-  function logOut() {
-    return signOut(auth);
+  function logOut ()
+  {
+    localStorage.removeItem( "role" );
+    return signOut( auth );
   }
-  function googleSignIn() {
+  function googleSignIn ()
+  {
     const googleAuthProvider = new GoogleAuthProvider();
-    return signInWithPopup(auth, googleAuthProvider);
+    return signInWithPopup( auth, googleAuthProvider );
   }
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
-      console.log("Auth", currentuser);
-      setUser(currentuser);
-    });
+  useEffect( () =>
+  {
+    const unsubscribe = onAuthStateChanged( auth, ( currentuser ) =>
+    {
+      console.log( "Auth", currentuser );
+      setUser( currentuser );
+    } );
 
-    return () => {
+    return () =>
+    {
       unsubscribe();
     };
-  }, []);
+  }, [] );
 
   return (
     <userAuthContext.Provider
-      value={{ user, logIn, signUp, logOut, googleSignIn }}
+      value={ { user, logIn, signUp, logOut, googleSignIn } }
     >
-      {children}
+      { children }
     </userAuthContext.Provider>
   );
 }
 
-export function useUserAuth() {
-  return useContext(userAuthContext);
+export function useUserAuth ()
+{
+  return useContext( userAuthContext );
 }
