bbd6238bd0371a2e42f1afeeb91cfbdbc47ebbe2
Vasu Mandhanya <55338336+Vasu-Mandhanya@users.noreply.github.com>
2022-06-05 06:28:52 +0530

    feat: Add cards to donor page.

--- src/pages/DonorPage.js
+++ src/pages/DonorPage.js
@@ -3,9 +3,12 @@ import { BarComponent } from "../components/BarComponent";
 import { PieChartComponents } from "../components/PieChartComponents";
 import { HorizontalBarComponent } from "../components/HorizontalBarComponent";
 import { FormComponent } from "../components/FormComponent";
+import { Card, Row } from 'react-bootstrap';
 import React from "react";
 import "./DonorPage.css";
-import {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import
+{
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
@@ -69,7 +72,8 @@ ChartJS.register(
 //   ],
 // };
 
-export default function App() {
+export default function App ()
+{
   const labels = [
     "January",
     "February",
@@ -97,12 +101,12 @@ export default function App() {
     datasets: [
       {
         label: "Dataset 1",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        data: labels.map( () => faker.datatype.number( { min: 0, max: 1000 } ) ),
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
       {
         label: "Dataset 2",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        data: labels.map( () => faker.datatype.number( { min: 0, max: 1000 } ) ),
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
     ],
@@ -119,9 +123,9 @@ export default function App() {
       45045, 56450, 65004, 65789, 76001, 77891, 89001, 92000, 92300, 94000,
       95032, 96034,
     ],
-    [235, 435, 356, 456, 567, 657, 567, 357, 645, 598, 700, 679],
-    [45678, 34567, 30000, 28000, 22203, 15000],
-    [4, 5, 6, 7, 5, 6, 5, 5, 6, 7, 8, 7],
+    [ 235, 435, 356, 456, 567, 657, 567, 357, 645, 598, 700, 679 ],
+    [ 45678, 34567, 30000, 28000, 22203, 15000 ],
+    [ 4, 5, 6, 7, 5, 6, 5, 5, 6, 7, 8, 7 ],
   ];
 
   const dataPlot = [
@@ -129,8 +133,8 @@ export default function App() {
       labels,
       datasets: [
         {
-          label: programs[0],
-          data: programData[0],
+          label: programs[ 0 ],
+          data: programData[ 0 ],
           backgroundColor: "rgba(255, 99, 132, 0.5)",
         },
       ],
@@ -139,8 +143,8 @@ export default function App() {
       labels,
       datasets: [
         {
-          label: programs[1],
-          data: programData[1],
+          label: programs[ 1 ],
+          data: programData[ 1 ],
           backgroundColor: "rgba(255, 99, 132, 0.5)",
         },
       ],
@@ -149,8 +153,8 @@ export default function App() {
       labels: labels_alt,
       datasets: [
         {
-          label: programs[2],
-          data: programData[2],
+          label: programs[ 2 ],
+          data: programData[ 2 ],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
@@ -175,8 +179,8 @@ export default function App() {
       labels,
       datasets: [
         {
-          label: programs[3],
-          data: programData[3],
+          label: programs[ 3 ],
+          data: programData[ 3 ],
           borderColor: "rgb(255, 99, 132)",
           backgroundColor: "rgba(255, 99, 132, 0.5)",
         },
@@ -186,27 +190,104 @@ export default function App() {
   return (
     <div className="donor-body">
       <NavbarComponent />
+      <p
+        style={ {
+          textAlign: "center",
+          margin: "20px 20px",
+          fontWeight: "400",
+          fontSize: "x-medium",
+        } }
+      >
+        <Row className="row1">
+          <Card
+            bg="primary"
+            key="primary"
+            text='white'
+            style={ { width: '18rem' } }
+            className="card1"
+          >
+            <Card.Header> #FUNDING </Card.Header>
+            <Card.Body>
+              <Card.Title> Rs. 1000000 </Card.Title>
+              <Card.Text>
+                Funding Recieved through charitable donations
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+
+          <Card
+            bg="success"
+            key="primary"
+            text='white'
+            style={ { width: '18rem' } }
+            className="card1"
+          >
+            <Card.Header> #WAKE THE LAKE</Card.Header>
+            <Card.Body>
+              <Card.Title> 50 Lakes</Card.Title>
+              <Card.Text>
+                Garbage collection and water sanitation to revive urban ecosystem.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card
+            bg="danger"
+            key="primary"
+            text='white'
+            style={ { width: '18rem' } }
+            className="card1"
+          >
+            <Card.Header> #RURAL RISING </Card.Header>
+            <Card.Body>
+              <Card.Title> 500 Villages </Card.Title>
+              <Card.Text>
+                Bridging the gap between rural and urban areas of Bangaluru.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card
+            bg="warning"
+            key="primary"
+            text='white'
+            style={ { width: '18rem' } }
+            className="card1"
+          >
+            <Card.Header> #BORN LEARNING CAMPAIGN
+            </Card.Header>
+            <Card.Body>
+              <Card.Title> 1000 Children </Card.Title>
+              <Card.Text>
+                Building awareness and foster holistic growth among children (0-6 years)
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Row>
+
+      </p>
       <FormComponent />
       <p
-        style={{
+        style={ {
           textAlign: "center",
           margin: "20px 20px",
           fontWeight: "500",
           fontSize: "x-large",
-        }}
+        } }
       >
-        {/* <hr /> */}
+        {/* <hr /> */ }
         <hr />
         IMPACT FIGURES AND VISUALIZATION
       </p>
-      <BarComponent data={dataPlot[0]} heading={programs[0]} />
+      <BarComponent data={ dataPlot[ 0 ] } heading={ programs[ 0 ] } />
       <br />
-      <BarComponent data={dataPlot[1]} heading={programs[1]} />
+      <BarComponent data={ dataPlot[ 1 ] } heading={ programs[ 1 ] } />
       <br />
       <p className="bis-title">STUDENTS HAVING ACCESS TO EDUCATION</p>
-      <PieChartComponents data={dataPlot[2]} />
+      <PieChartComponents data={ dataPlot[ 2 ] } />
       <br />
-      <HorizontalBarComponent data={dataPlot[3]} heading={programs[3]} />
+      <HorizontalBarComponent data={ dataPlot[ 3 ] } heading={ programs[ 3 ] } />
     </div>
   );
 }
