a8a715b6b15145298248bc426a6013d4795b9a02
Vasu Mandhanya <55338336+Vasu-Mandhanya@users.noreply.github.com>
2022-06-05 07:12:06 +0530

    Fix calendar background

--- src/components/ManagementMilestonesComponent.js
+++ src/components/ManagementMilestonesComponent.js
@@ -15,23 +15,23 @@ const ManagementMilestonesComponent = ( programName ) =>
     const [ milestoneDate, setMilestoneDate ] = useState( new Date() );
     const [ error, setError ] = useState( "" );
     const [ milestones, setMilestones ] = useState( [] )
-    const [ ganttTasks, setGanttTasks ] = useState( [] )
-    useEffect( () =>
-    {
-        let tasks = []
-        milestones.forEach( ( item, index ) =>
-        {
-            var start = new Date( item.date )
-            var t = start.getDate() - 7;
-            start = start.setDate( t )
-            start = new Date( start )
-            tasks = [ ...tasks, { start: index === 0 ? start : new Date( milestones[ index - 1 ].date ) < new Date( item.date ) ? new Date( milestones[ index - 1 ].date ) : start, end: new Date( item.date ), name: item.title, id: "Task " + ( index + 1 ), styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' } } ]
-        } );
-        console.log( tasks )
-        setGanttTasks( tasks )
-        console.log( ganttTasks )
-    }
-        , [ milestones ] )
+    // const [ ganttTasks, setGanttTasks ] = useState( [] )
+    // useEffect( () =>
+    // {
+    //     let tasks = []
+    //     milestones.forEach( ( item, index ) =>
+    //     {
+    //         var start = new Date( item.date )
+    //         var t = start.getDate() - 7;
+    //         start = start.setDate( t )
+    //         start = new Date( start )
+    //         tasks = [ ...tasks, { start: index === 0 ? start : new Date( milestones[ index - 1 ].date ) < new Date( item.date ) ? new Date( milestones[ index - 1 ].date ) : start, end: new Date( item.date ), name: item.title, id: "Task " + ( index + 1 ), styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' } } ]
+    //     } );
+    //     console.log( tasks )
+    //     setGanttTasks( tasks )
+    //     console.log( ganttTasks )
+    // }
+    //     , [ milestones ] )
     const handleNewMilestone = async ( e ) =>
     {
         e.preventDefault();
@@ -85,13 +85,13 @@ const ManagementMilestonesComponent = ( programName ) =>
                 </Button>
 
             </form>
-            <h2>
+            {/* <h2>
                 Gantt Chart
-            </h2>
-            {
+            </h2> */}
+            {/* {
                 ganttTasks.length > 0 ?
                     <Gantt tasks={ ganttTasks } /> : <p>Please add a milestone.</p>
-            }
+            } */}
             <h2>
                 Milestones Calendar
             </h2>
@@ -99,11 +99,15 @@ const ManagementMilestonesComponent = ( programName ) =>
                 milestones.map( ( val ) => { <p>{ val.title } on  { val.date } </p> } )
             } */}
 
-            <FullCalendar
-                plugins={ [ dayGridPlugin ] }
-                initialView="dayGridMonth"
-                events={ milestones }
-            />
+            <div className="Calendar">
+
+
+                <FullCalendar
+                    plugins={ [ dayGridPlugin ] }
+                    initialView="dayGridMonth"
+                    events={ milestones }
+                />
+            </div>
 
             <Button variant="primary" className="signupButton submitMilestone" onClick={ handleSubmit }>
                 Submit Milestones
